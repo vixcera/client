9d8feab65be5447c2289af249975b63ecf7e72fa
vixcera <vixceraa@gmail.com>
2023-10-27 14:47:07 +0700

    add comment fitur

--- src/pages/support.jsx
+++ src/pages/support.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import Safe from "react-safe"
+import { Comments } from "@hyvor/hyvor-talk-react";
 
 const Support = () => {
 
@@ -11,17 +11,9 @@ const Support = () => {
                 <div className="fa-solid fa-arrow-left fa-xl active"></div>
                 <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
             </div>
-            <div className="product-page" style={(window.innerWidth < 425) ? {marginTop: '20px'} : {marginTop: '0'}}>
-                <div id="disqus_thread"></div>
+            <div className="product-page" style={{color: 'var(--yellow)', fontFamily: 'var(--poppins)',  marginTop: '30px'}}>
+               <Comments website-id={9873}/>
             </div>
-            <Safe.script>
-                {(function() { // DON'T EDIT BELOW THIS LINE
-                    var d = document, s = d.createElement('script');
-                    s.src = 'https://vixcera-netlify-app.disqus.com/embed.js';
-                    s.setAttribute('data-timestamp', +new Date());
-                    (d.head || d.body).appendChild(s);
-                })()}
-            </Safe.script>
         </div>
     )
 }
