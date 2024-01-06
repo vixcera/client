27f1d3f455ccbdc824e79149f0be04b86f27e8a7
vixcera <vixceraa@gmail.com>
2024-01-06 22:08:40 +0700

    update (still develop)

--- src/pages/user/main.jsx
+++ src/pages/user/main.jsx
@@ -1,20 +1,15 @@
 import Navbar from "../../components/navbar"
 import Content from "../../components/content"
 import Sidebar from "../../components/sidebar"
-import Loading from "../../../utils/loading"
-import { Suspense } from "react"
 import "../../style/main.css"
 
 const Main = () => {
 
     return (
         <div className="main">
-            <Suspense fallback={<Loading/>}>
                 <Navbar/>
                 <Content/>
                 <Sidebar/>
-            </Suspense>
-            
         </div>
     )
 }
