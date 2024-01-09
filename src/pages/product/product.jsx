a40fb13c51a95a3b9d7b3f59083a7b0dc93b1fb1
vixcera <vixceraa@gmail.com>
2024-01-09 15:11:42 +0700

    update (still develop)

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -61,18 +61,23 @@ const Product = () => {
             </div>
             <div className='product-page'>
                 <div className='product-container' style={(data.length < 4) && {justifyContent: 'center'}}>
-                    <div onClick={() => requestCreate()} className='product-card' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', flexDirection: 'column'}}>
+                    {/* <div onClick={() => requestCreate()} className='product-card' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', flexDirection: 'column'}}>
                         <i className='fa-solid fa-plus fa-2xl' style={{fontSize: '80px', color: 'var(--yellow)'}}/>
                         <div style={{color: 'var(--yellow)', fontFamily: 'var(--poppins)', fontSize: '1rem', translate: '0 50px'}}>add new product</div>
-                    </div>
+                    </div> */}
                     {data.map((i, k) => {
                             return(
                             <div className='product-card' key={k}>
                                 <LazyLoadImage className='product-img' src={i.img} loading='lazy' effect='blur'/>
-                                <div className='product-title'>{i.title}</div>
-                                <div className='product-desc'>{i.desc}</div>
-                                <div className='button' style={{width : '140px', position:'absolute', bottom:'15px', left:'15px'}}>{convertPrice(i.price)}</div>
-                                <div style={{ position:'absolute', bottom:'30px', right:'20px', color : 'var(--text)', cursor: 'pointer'}} onClick={() => navigate(`/order/${i.id}`)} className='fa-solid fa-cart-plus fa-xl' />
+                                <div className='wrapped-text'>
+                                    <div className='product-title'>{i.title}</div>
+                                    <div className='product-desc'>{i.desc}</div>
+                                    <div className='wrapped-details'>
+                                        <div className='button' style={{width : '140px'}}>{convertPrice(i.price)}</div>
+                                        <div style={{ color : 'var(--text)', cursor: 'pointer'}} onClick={() => navigate(`/order/${i.id}`)} className='fa-solid fa-cart-plus fa-xl' />
+                                    </div>
+                                </div>
+                                <div className='by'>by: {i.by}</div>
                             </div>
                             )
                         })
