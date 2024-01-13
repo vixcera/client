674e26d0954b12ec63599f1643e9f1f9f3e8db15
vixcera <vixceraa@gmail.com>
2024-01-14 00:56:59 +0700

    update (still develop)

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -93,7 +93,7 @@ const Content = () => {
                 <div className="text-wrapper">
                 <div>Become a Contributor</div>
                 <div>Sell your best work</div>
-                <div className="button contact">Get started</div>
+                <div className="button contact">Coming Soon</div>
                 </div>
                 </div>
                 {(products.map((i,k) => {
