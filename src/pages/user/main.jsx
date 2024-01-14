8754ba4ff131d6f52daf9050d8b04d473de480c6
vixcera <vixceraa@gmail.com>
2024-01-15 05:49:12 +0700

    update (still develop)

--- src/pages/user/main.jsx
+++ src/pages/user/main.jsx
@@ -1,9 +1,13 @@
 import Navbar from "../../components/navbar"
 import Content from "../../components/content"
 import Sidebar from "../../components/sidebar"
+import { useEffect } from "react"
 import "../../style/main.css"
+import browser from "../../../utils/browser"
 
 const Main = () => {
+
+    useEffect(() => { browser()}, [])
 
     return (
         <div className="main">
