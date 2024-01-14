ab0c98b24e95dd87358a456ec55c17441644cc96
vixcera <vixceraa@gmail.com>
2024-01-14 15:22:17 +0700

    update (still develop)

--- src/pages/product/create.jsx
+++ src/pages/product/create.jsx
@@ -46,7 +46,7 @@ const Create = () => {
 
 
   return (
-    <div className='page' style={{gap:'30px'}}>
+    <div className='page-max' style={{gap:'30px'}}>
       <div className="back" onClick={() => navigate('/')}>
         <div className="fa-solid fa-arrow-left fa-xl active"></div>
         <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
