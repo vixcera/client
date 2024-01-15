b5c66e871c241f2cd326dbaa1d1f0e53ebca208b
vixcera <vixceraa@gmail.com>
2024-01-15 15:12:55 +0700

    update (still develop)

--- utils/browser.js
+++ utils/browser.js
@@ -11,48 +11,48 @@ const browser = async () => {
     const useragent = navigator.userAgent
     const localagent = sessionStorage.getItem("useragent")
     const urldocs = 'https://support.apple.com/en-gb/guide/iphone/iphb01fc3c85/ios#:~:text=Control%20privacy%20and%20security%20settings,to%20allow%20cross%2Dsite%20tracking.'
-    
 
-    // if (!localagent) {
-    //     if (!thirdCookie) {
-    //         swal.fire({
-    //             icon: 'info',
-    //             titleText: 'safari detected!',
-    //             text: "vixcera requires third-party cookies, please turn off prevent cross-site tracking on safari settings.",
-    //             color: '#ccc',
-    //             background: 'var(--primary)',
-    //             confirmButtonText : "what's happend?",
-    //             showDenyButton: true,
-    //             denyButtonText : 'later'
-    //         })
-    //         .then((response) => {
-    //             if (response.dismiss || response.isDenied) {
-    //                 swal.fire({
-    //                     icon: "warning",
-    //                     text: "vixcera may not work properly on this browser",
-    //                     background: 'var(--primary)',
-    //                     color : '#ccc',
-    //                     showDenyButton: true,
-    //                     denyButtonText: 'skip',
-    //                     confirmButtonText : "let's do it"
-    //                 })
-    //                 .then((res) => {
-    //                     if (res.isConfirmed)  {
-    //                         sessionStorage.setItem("useragent", useragent)
-    //                         window.open(urldocs)
-    //                     } else {
-    //                         sessionStorage.setItem("useragent", useragent)
-    //                     }
-    //                 })
-    //             }
-    //             if (response.isConfirmed) {
-    //                 sessionStorage.setItem("useragent", useragent)
-    //                 window.open(urldocs)
-    //             }
-    //         })
-    //     }
-    // }
-    // return;
+
+    if (!localagent) {
+        if (!brow.satisfies({ safari : ">= 10" })) {
+            swal.fire({
+                icon: 'info',
+                titleText: 'safari detected!',
+                text: "vixcera requires third-party cookies, please turn off prevent cross-site tracking on safari settings.",
+                color: '#ccc',
+                background: 'var(--primary)',
+                confirmButtonText : "what's happend?",
+                showDenyButton: true,
+                denyButtonText : 'later'
+            })
+            .then((response) => {
+                if (response.dismiss || response.isDenied) {
+                    swal.fire({
+                        icon: "warning",
+                        text: "vixcera may not work properly on this browser",
+                        background: 'var(--primary)',
+                        color : '#ccc',
+                        showDenyButton: true,
+                        denyButtonText: 'skip',
+                        confirmButtonText : "let's do it"
+                    })
+                    .then((res) => {
+                        if (res.isConfirmed)  {
+                            sessionStorage.setItem("useragent", useragent)
+                            window.open(urldocs)
+                        } else {
+                            sessionStorage.setItem("useragent", useragent)
+                        }
+                    })
+                }
+                if (response.isConfirmed) {
+                    sessionStorage.setItem("useragent", useragent)
+                    window.open(urldocs)
+                }
+            })
+        }
+    }
+    return;
 }
 
 export default browser
