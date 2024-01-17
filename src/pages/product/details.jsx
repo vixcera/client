496301e0f74e49c9fceea6e30335416ea0efbed7
vixcera <vixceraa@gmail.com>
2024-01-18 00:14:57 +0700

    update (still develop)

--- src/pages/product/details.jsx
+++ src/pages/product/details.jsx
@@ -16,6 +16,7 @@ const Details = () => {
     const navigate = useNavigate()
     const [data, setData] = useState([])
     const img = data.map((i) => { return i.img })
+    const w = window.innerWidth;
 
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_API}/products/vid/${vid}`)
@@ -29,28 +30,33 @@ const Details = () => {
                 <div className="fa-solid fa-arrow-left fa-xl active"></div>
                 <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
             </div>
-            <div className='prev-form' style={{ marginTop: '50px' }}>
-              <div className='itext'>Product Details</div>
-              <div className="product-card" style={{alignItems: 'center', justifyContent:'center', padding: '10px'}}>
-                <LazyLoadImage onClick={() => swal.fire({ imageUrl: img, showConfirmButton: false })} className='product-img' src={img} loading='lazy' effect='blur'/>
-              </div>
-              {data.map((i,k) => {
-                  return(
-                      <div className='product-card' style={{ height: 'max-content' }} key={k}>
-                          <div className='wrapped-text'>
-                              <div className='product-title'>{i.title}</div>
-                              <div className='product-desc' style={{ display: "block" }}>{i.desc}</div>
-                              <div className='wrapped-details' style={{margin: 0, paddingTop: '25px', display: 'flex',alignItems: 'unset', flexDirection: "column", gap: '8px'}}>
-                                  <div className="product-desc-product">Price       : <span>{convertPrice(i.price)}</span></div>
-                                  <div className="product-desc-product">Category    : <span>{i.ctg}</span></div>
-                                  {i.ctg == 'web' && <div className="product-desc-product">Technology  : <span>{i.tech || '-'}</span></div>}
-                                  <div className="product-desc-product">Type        : <span>{i.type || '-'}</span></div>
-                                  <div className="product-desc-product">Created by  : <span>{i.by}</span></div>
-                              </div>
-                          </div>
-                      </div>
-                    )
-                })}
+            <div className="form">
+                <div className='prev-form' style={{ marginTop: '10px', paddingBottom: '0' }}>
+                    <div className='itext'>Product Details</div>
+                    <div className="product-card" style={{ height: 'max-content', width: '100%', marginTop: '10px', justifyContent: 'center' }}>
+                        <LazyLoadImage style={{ width: '100%' }} onClick={() => swal.fire({ imageUrl: img, showConfirmButton: false })} className='product-img' src={img} loading='lazy' effect='blur'/>
+                    </div>
+                {data.map((i,k) => {
+                    return(
+                        <>
+                        <div className='product-card' style={{ height: 'max-content', width: '100%' }} key={k}>
+                            <div className='wrapped-text'>
+                                <div className='product-title'>{i.title}</div>
+                                <div className='product-desc' style={{ display: "block" }}>{i.desc}</div>
+                                <div className='wrapped-details' style={{margin: 0, display: 'flex',alignItems: 'unset', flexDirection: "column", gap: '10px'}}>
+                                    <div className="product-desc-product">Price       : <span>{convertPrice(i.price)}</span></div>
+                                    <div className="product-desc-product">Category    : <span>{i.ctg}</span></div>
+                                    {i.ctg == 'web' && <div className="product-desc-product">Technology  : <span>{i.tech || '-'}</span></div>}
+                                    <div className="product-desc-product">Type        : <span>{i.type || '-'}</span></div>
+                                    <div className="product-desc-product">Created by  : <span>{i.by}</span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="button-max" style={{ marginTop: '30px' }}>Buy now</div>
+                        </>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
