bb423b349f873d308078be684ca288fe610f7a39
vixcera <vixceraa@gmail.com>
2024-01-17 22:08:29 +0700

    update (still develop)

--- src/pages/product/details.jsx
+++ src/pages/product/details.jsx
@@ -32,7 +32,7 @@ const Details = () => {
             <div className='prev-form' style={{ marginTop: '50px' }}>
               <div className='itext'>Product Details</div>
               <div className="product-card" style={{alignItems: 'center', justifyContent:'center'}}>
-                <LazyLoadImage style={{ height: '100%' }} onClick={() => swal.fire({ imageUrl: img, showConfirmButton: false })} className='product-img' src={img} loading='lazy' effect='blur'/>
+                <LazyLoadImage style={{ height: '210px' }} onClick={() => swal.fire({ imageUrl: img, showConfirmButton: false })} className='product-img' src={img} loading='lazy' effect='blur'/>
               </div>
               {data.map((i,k) => {
                   return(
