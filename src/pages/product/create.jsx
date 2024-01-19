24056af046f59043c0f2ed8cc8d7128ccd0ab666
vixcera <vixceraa@gmail.com>
2024-01-19 13:24:38 +0700

    update (still develop)

--- src/pages/product/create.jsx
+++ src/pages/product/create.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import {LazyLoadImage} from "react-lazy-load-image-component"
 import convertPrice from '../../../utils/price'
