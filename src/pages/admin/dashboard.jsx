5b0901c05ae4a6ae88243c1bd4bfcc50a5a154fd
vixcera <vixceraa@gmail.com>
2024-01-21 15:33:03 +0700

    update (still develop)

--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -48,7 +48,7 @@ const Dashboard = () => {
     
     return (
         <div className='page-max'>
-            <div className="back" onClick={() => navigate('/')}>
+            <div className="back" onClick={() => location.href = '/'}>
                 <div className="fa-solid fa-arrow-left fa-xl active"></div>
                 <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
             </div>
