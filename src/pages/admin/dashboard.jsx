100555c63077d4e12867fcd9c87c516ef7fee183
vixcera <vixceraa@gmail.com>
2024-01-21 15:26:40 +0700

    update (still develop)

---
+++ src/pages/admin/dashboard.jsx
@@ -0,0 +1,86 @@
+import axios from 'axios'
+import swal from "sweetalert2"
+import convertPrice from "../../../utils/price"
+import getvxsrf from "../../../secure/getvxsrf"
+import { useEffect, useState } from 'react'
+import { Form, useNavigate } from 'react-router-dom'
+import {LazyLoadImage} from "react-lazy-load-image-component"
+
+const Dashboard = () => {
+    const navigate = useNavigate()
+    const [ data, setData ] = useState([])
+    const [ password, setPassword ] = useState('')
+    const vxpwd = sessionStorage.getItem("vxpwd")
+    
+    const checkAdmin = async () => {
+        const input = await swal.fire({input: 'password', inputValue: vxpwd ? vxpwd : '' })
+        if (!input.value) return navigate('/')
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_API}/products/waitinglist`, {
+                headers: { "author" : input.value }
+            })
+            setPassword(input.value)
+            setData(response.data)
+            sessionStorage.setItem("vxpwd", input.value)
+        } 
+            catch (error) {
+            if (error) return navigate('/') 
+        }
+    }
+    
+    const confirm = async () => {
+        const response = await axios.post(`${import.meta.env.VITE_API}/product/confirm`,{
+            password: password,
+            vid: data.map((i) => {return i.vid})
+        })
+        swal.fire({icon:'success', showConfirmButton:false,timer:1500,text:response.data})
+    }
+
+    const reject = async () => {
+        const response = await axios.post(`${import.meta.env.VITE_API}/product/reject`,{
+            password: password,
+            vid: data.map((i) => {return i.vid})
+        })
+        swal.fire({icon:'success', showConfirmButton:false,timer:1500,text:response.data})
+    }
+
+    useEffect(() => checkAdmin(), [])
+    
+    return (
+        <div className='page-max'>
+            <div className="back" onClick={() => navigate('/')}>
+                <div className="fa-solid fa-arrow-left fa-xl active"></div>
+                <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
+            </div>
+            <div className='product-page'>
+                <div className='product-container'>
+                <input type="text" className='search'/>
+                    {data.map((i, k) => {
+                            return(
+                            <div className='product-card' key={k}>
+                                <LazyLoadImage className='product-img' onClick={() => navigate(`/waiting/details/${i.vid}`)} src={(i.img) || ('img/img404.jpg')} loading='lazy' effect='blur'/>
+                                <div className='wrapped-text'>
+                                    <div className='product-title'>{i.title}</div>
+                                    <div style={{ display: 'flex', flexWrap : 'wrap', flexDirection : 'column'}}>
+                                        <div className='product-desc'>{i.desc.length >= 40 ? i.desc.substring(0,40) + '...' : i.desc}</div>
+                                        <div className='wrapdet' style={{ position: 'unset', marginTop: '15px', marginLeft: '5px', gap: '5px' }}>
+                                            <div style={{ backgroundColor: 'var(--background)', width: '95px', height: '30px' }}>{i.tech}</div>
+                                            <div style={{ backgroundColor: 'var(--background)', width: '95px', height: '30px' }}>{i.tech.toLowerCase().includes('html') ? "only" : 'JS'}</div>
+                                         </div>
+                                    </div>
+                                    <div className='wrapped-details'>
+                                        <div className='button price'>{convertPrice(i.price)}</div>
+                                        <div style={{ color : 'var(--text)', cursor: 'pointer'}} onClick={() => navigate(`/order/${i.vid}`)} className='fa-solid fa-cart-plus fa-xl' />
+                                    </div>
+                                </div>
+                            </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ src/pages/admin/wetails.jsx
@@ -0,0 +1,104 @@
+import axios from "axios"
+import swal from "sweetalert2"
+import convertPrice from "../../../utils/price"
+
+import { useState } from "react"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { LazyLoadImage } from "react-lazy-load-image-component"
+
+import "../../style/create.css"
+
+const Wetails = () => {
+
+    const navigate = useNavigate()
+    const { vid } = useParams()
+    const vxpwd = sessionStorage.getItem("vxpwd")
+    const [data, setData] = useState([])
+    const img = data.map((i) => { return i.img })
+    const width = window.innerWidth;
+
+    const checkAdmin = async () => {
+        const result = await swal.fire({
+          title: 'Admin Password',
+          input: 'password',
+          inputValue : vxpwd? vxpwd : '',
+          inputPlaceholder: 'Enter your password',
+          showCancelButton: true,
+          preConfirm: async (password) => {
+            if (!password) {
+              swal.showValidationMessage('Password is required');
+              return false;
+            }
+      
+            try {
+              const response = await axios.post(`${import.meta.env.VITE_API}/products/waitinglist`, { password });
+              setData(response.data);
+              sessionStorage.setItem('vxpwd', password);
+              return true;
+            } catch (error) {
+              swal.showValidationMessage('Invalid password');
+              return false;
+            }
+          },
+        });
+      
+        if (result.isDismissed || !result.value) {
+          // Jika dibatalkan atau password tidak diisi, arahkan ke halaman lain
+          navigate('/');
+        }
+      };
+      
+    useEffect(() => checkAdmin())
+    return (
+        <div className='page-max'>
+            <div className="back" onClick={() => navigate(-1)}>
+                <div className="fa-solid fa-arrow-left fa-xl active"></div>
+                <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
+            </div>
+            <div className="form">
+                <div className='prev-form' style={{ marginTop: '10px', paddingBottom: '0', gap: '20px' }}>
+                    <div className='itext'>Product Details</div>
+                    <div className="product-card" style={{ height: 'max-content', width: '100%', marginTop: '0px', justifyContent: 'center' }}>
+                        <LazyLoadImage style={{ width: '100%' }} onClick={() => (width) <= 530 && swal.fire({ imageUrl: img, showConfirmButton: false })} className='product-img' src={img} loading='lazy' effect='blur'/>
+                    </div>
+                {data.map((i,k) => {
+                    return(
+                        <>
+                        <div className='product-card' style={{ height: 'max-content', width: '100%' }} key={k}>
+                            <div className='wrapped-text'>
+                                <div className='product-title'>{i.title}</div>
+                                <div className='product-desc' style={{ display: "block" }}>{i.desc}</div>
+                            </div>
+                        </div>
+                        <div className='product-card' style={{ height: 'max-content', width: '100%', marginTop: '10px' }}>
+                            <div className='wrapped-text'>
+                                <div className='wrapped-details' style={{margin: 0, paddingTop: '0', display: 'flex',alignItems: 'unset', flexDirection: "column", gap: '10px'}}>
+                                    <div className="product-desc-product">VID         : <span>{i.vid}</span></div>
+                                    <div className="product-desc-product">Price       : <span>{convertPrice(i.price)}</span></div>
+                                    <div className="product-desc-product">Category    : <span>{i.ctg}</span></div>
+                                    {i.ctg == 'web' && <div className="product-desc-product">Framework  : <span>{i.tech} {i.tech.toLowerCase().includes('html') ? "" : 'JS'}</span></div>}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='product-card' style={{ height: 'max-content', width: '100%' }}>
+                            <div className='wrapped-text'>
+                                <div className='wrapped-details' style={{margin: 0, paddingTop: '0', display: 'flex',alignItems: 'unset', flexDirection: "column", gap: '10px'}}>
+                                    <div className="product-desc-product">Created by  : <span>{i.by}</span></div>
+                                    <div className="product-desc-product">Created at  : <span>{i.createdAt.slice(0, 10)}</span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="button-max" style={{ marginTop: '30px', fontWeight: 'bold', backgroundColor: 'var(--yellow)' }}>Approve</div>
+                        <div className="button-max" style={{ marginTop: '5px', fontWeight: 'bold', backgroundColor: '#aaa' }}>Reject</div>
+                        </>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Wetails
