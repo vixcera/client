1e46e2252eb1de87262e8266eb436f280d333d63
vixcera <vixceraa@gmail.com>
2024-01-23 20:54:29 +0700

    update (still develop)

--- src/pages/product/create.jsx
+++ src/pages/product/create.jsx
@@ -64,7 +64,7 @@ const Create = () => {
   useEffect(() => { getvxsrf().then((data) => setVxsrf(data)) }, [])
 
   return (
-    <div className='page-max' style={{gap:'30px'}}>
+    <div className='page-max' style={{gap:'30px', paddingBottom: '5px'}}>
       <div className="back" onClick={() => navigate('/')}>
         <div className="fa-solid fa-arrow-left fa-xl active"></div>
         <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
@@ -132,7 +132,7 @@ const Create = () => {
                 <div style={{ color: '#aaa', fontSize: '0.95rem' }}>Max size: 20 Mb</div>
               </div>
             </div>
-            <div className='button-max' onClick={() => createProduct()} style={(file && title && image && desc && price && ctg && tech) ? {backgroundColor: 'var(--yellow)', marginTop: '60px'} : {backgroundColor: '#aaa', marginTop: '60px'}}>Create</div>
+            <div className='button-max' onClick={() => createProduct()} style={(file && title && image && desc && price && ctg && tech) ? {backgroundColor: 'var(--yellow)', marginTop: '50px'} : {backgroundColor: '#aaa', marginTop: '50px'}}>Create</div>
             </div>
           </div>
         <div className='prev-form'>
