edaa26a8f440f1e2de8a0461b75e56afa8c8bb8d
vixcera <vixceraa@gmail.com>
2024-01-23 13:10:28 +0700

    update (still develop)

--- data/product.jsx
+++ data/product.jsx
@@ -21,7 +21,7 @@ const products =
             {
                 title : "Motion graphic",
                 text : "Motion logo templates",
-                img : "img/motions.png",
+                img : "img/motion.png",
                 color : "var(--yellow)",
                 pricing : ["paid", "free"],
                 ctg : "motion"
