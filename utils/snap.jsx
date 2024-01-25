61ee263dd272a8b44185a12ef5c8d2ceb256692e
vixcera <vixceraa@gmail.com>
2024-01-25 18:44:25 +0700

    update (still develop)

--- utils/snap.jsx
+++ utils/snap.jsx
@@ -1,8 +1,8 @@
 const snap =  async () => {
-        const midtransScriptUrl = 'https://app.midtrans.com/snap/snap.js';  
+        const midtransScriptUrl = 'https://app.sandbox.midtrans.com/snap/snap.js';  
         let scriptTag = document.createElement('script');
         scriptTag.src = midtransScriptUrl;
-        scriptTag.setAttribute('data-client-key', import.meta.env.VITE_CLIENT_KEY);
+        scriptTag.setAttribute('data-client-key', import.meta.env.VITE_CLIENT_KEY_SANDBOX);
         document.body.appendChild(scriptTag);
         return () => {
           document.body.removeChild(scriptTag);
