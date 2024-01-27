6350d1a7423648676c75c6d57c87af97301d34c6
vixcera <vixceraa@gmail.com>
2024-01-27 18:25:23 +0700

    update (still develop)

--- src/pages/product/order.jsx
+++ src/pages/product/order.jsx
@@ -90,8 +90,8 @@ const Order = () => {
           <div className="back" onClick={() => navigate('/products')}>
             <div className="fa-solid fa-arrow-left fa-xl active"></div>
             <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
+            <div className='snap-container'></div>
           </div>
-          <div className='snap-container'></div>
           <div className='form'>
             <div className='input-form' >
               <div>
