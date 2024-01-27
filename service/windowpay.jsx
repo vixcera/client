9bf95699fb5b46962b70d04893bc5da3f04ecf17
vixcera <vixceraa@gmail.com>
2024-01-27 13:16:14 +0700

    update (still develop)

--- service/windowpay.jsx
+++ service/windowpay.jsx
@@ -1,20 +1,13 @@
 import { createStorage } from "../function/store"
 
 const windowpay = (token) => {
-  let endpoint = "https://vixcera.my.id/transaction/success"
     window.snap.pay(token, {
         onSuccess: (result) => {
           createStorage('transaction', token, `${endpoint}${result.finish_redirect_url}`, result.order_id, result.transaction_status, 5)
-          location.href = result.finish_redirect_url
         },
         onPending: (result) => {
           createStorage('transaction', token, `${endpoint}${result.finish_redirect_url}`, result.order_id, result.transaction_status, 5)
-          window.snap.hide()
         },
-        onClose: (result) => {
-          createStorage('transaction', token, `${endpoint}${result.finish_redirect_url}`, result.order_id, result.transaction_status, 5)
-          window.snap.hide()
-        }
     })
 } 
 
