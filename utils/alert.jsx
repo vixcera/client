f98b746606f441f4e6bed2ef31bc719c7de9d091
vixcera <vixceraa@gmail.com>
2024-01-28 20:55:07 +0700

    update (still develop)

--- utils/alert.jsx
+++ utils/alert.jsx
@@ -1,14 +1,14 @@
-import swal from "sweetalert2"
+import swal from "sweetalert2";
 
 const alert = (error) => {
-    return swal.fire({
-        icon: "info",
-        text: `${error}`,
-        showConfirmButton : false,
-        color : 'var(--blue)',
-        background: 'var(--primary)',
-        timer: 1500
-    })
-}
+  return swal.fire({
+    icon: "info",
+    text: `${error}`,
+    showConfirmButton: false,
+    color: "var(--blue)",
+    background: "var(--primary)",
+    timer: 1500,
+  });
+};
 
-export default alert
+export default alert;
