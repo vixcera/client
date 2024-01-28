4a5e37ce9ae971aaf49366b6f6bd6f0fa1a070bb
vixcera <vixceraa@gmail.com>
2024-01-29 05:06:52 +0700

    update vixcera

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -2,11 +2,9 @@ import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import convertPrice from "../../../utils/price"
-import Loading from "../../../utils/loading"
 import swalert from '../../../utils/swalert'
-import Skeleton from 'react-loading-skeleton'
+import Loader from '../../../utils/loader'
 import axios from "axios"
-import 'react-loading-skeleton/dist/skeleton.css'
 import "../../style/product.css"
 
 const Product = () => {
@@ -37,8 +35,6 @@ const Product = () => {
     }
 
     useEffect(() => { getProducts() }, [])
-    
-    if (loading) return <Loading/>
 
     return (
         <div className='page-max'>
@@ -50,29 +46,31 @@ const Product = () => {
             <div className='product-page'>
                 <div className='product-container'>
                     <input type="text" className='search'/>
-                    <Skeleton width={200} height={30} baseColor='var(--second)'/>
-                    {data.map((i, k) => {
+                    {(loading) ? (
+                    <Loader.Product/>
+                    ) : (
+                        data.map((i, index) => {
                             return(
-                            <div className='product-card' key={k} onClick={() => navigate(`/product/details/${i.vid}`)}>
-                                <LazyLoadImage className='product-img' src={(i.img) || ('img/img404.jpg')} loading='lazy' effect='blur'/>
-                                <div className='wrapped-text'>
-                                    <div className='product-title'>{i.title}</div>
-                                    <div style={{ display: 'flex', flexWrap : 'wrap', flexDirection : 'column'}}>
-                                        <div className='product-desc'>{i.desc.length >= 40 ? i.desc.substring(0,40) + '...' : i.desc}</div>
-                                        <div className='wrapdet' style={{ position: 'unset', marginTop: '15px', marginLeft: '5px', gap: '5px' }}>
-                                            <div style={{ backgroundColor: 'var(--background)', width: '95px', height: '30px' }}>{i.tech}</div>
-                                            <div style={{ backgroundColor: 'var(--background)', width: '95px', height: '30px' }}>{i.tech.toLowerCase().includes('html') ? "only" : 'JS'}</div>
-                                         </div>
-                                    </div>
-                                    <div className='wrapped-details'>
-                                        <div className='button price'>{convertPrice(i.price)}</div>
-                                        <div style={{ color : 'var(--text)', cursor: 'pointer'}} className='fa-solid fa-cart-plus fa-xl' />
+                                <div className='product-card' key={index} onClick={() => navigate(`/product/details/${i.vid}`)}>
+                                    <LazyLoadImage className='product-img' src={(i.img) || ('img/img404.jpg')} loading='lazy' effect='blur'/>
+                                    <div className='wrapped-text'>
+                                        <div className='product-title'>{i.title}</div>
+                                        <div style={{ display: 'flex', flexWrap : 'wrap', flexDirection : 'column'}}>
+                                            <div className='product-desc'>{i.desc.length >= 40 ? i.desc.substring(0,40) + '...' : i.desc}</div>
+                                            <div className='wrapdet' style={{ position: 'unset', marginTop: '15px', marginLeft: '5px', gap: '5px' }}>
+                                                <div style={{ backgroundColor: 'var(--background)', width: '95px', height: '30px' }}>{i.tech}</div>
+                                                <div style={{ backgroundColor: 'var(--background)', width: '95px', height: '30px' }}>{i.tech.toLowerCase().includes('html') ? "only" : 'JS'}</div>
+                                             </div>
+                                        </div>
+                                        <div className='wrapped-details'>
+                                            <div className='button price'>{convertPrice(i.price)}</div>
+                                            <div style={{ color : 'var(--text)', cursor: 'pointer'}} className='fa-solid fa-cart-plus fa-xl' />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            )
+                                )
                         })
-                    }
+                    )}
                 </div>
             </div>
         </div>
---
+++ utils/loader.jsx
@@ -0,0 +1,27 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css"
+const lenghtData = Array.from({ length: 5 }, () => ({}))
+
+const Loader = {
+    Product : () => {
+        return lenghtData.map((i, index) => {
+            return(
+            <div className='product-card' key={index}>
+            <Skeleton style={{boxShadow: 'var(--softshadow)'}} className='product-img' baseColor='var(--primary)' highlightColor='var(--prime)'/>
+            <div className='wrapped-text'>
+            <Skeleton  style={{boxShadow: 'var(--softshadow)'}} className='product-title' count={2} baseColor='var(--primary)' highlightColor='var(--prime)'/>
+            <div className='wrapdet' style={{ position: 'unset', marginTop: '15px', marginLeft: '5px', gap: '5px' }}>
+            <Skeleton style={{boxShadow: 'var(--softshadow)'}} width={95} height={30} baseColor='var(--primary)' highlightColor='var(--prime)'/>
+            <Skeleton style={{boxShadow: 'var(--softshadow)'}} width={95} height={30} baseColor='var(--primary)' highlightColor='var(--prime)'/>
+            </div>
+            <div className='wrapped-details'>
+            <Skeleton className='button price' style={{ backgroundColor: 'unset' }} baseColor='var(--primary)' highlightColor='var(--prime)'/>
+            </div>
+            </div>
+            </div>
+            )
+        } )
+    }
+}
+
+export default Loader
