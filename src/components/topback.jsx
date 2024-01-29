dc5997a53d153997514ee610597afc3e96b6c886
vixcera <vixceraa@gmail.com>
2024-01-29 19:35:50 +0700

    update vixcera

--- src/components/topback.jsx
+++ src/components/topback.jsx
@@ -5,7 +5,7 @@ const Topback = () => {
     let navigate = useNavigate()
 
     return(
-        <div className="back" onClick={() => navigate('/')}>
+        <div className="back" onClick={() => navigate('/products')}>
             <div className="fa-solid fa-arrow-left fa-xl active"></div>
             <div className="nav-logo"><p style={{fontFamily : "var(--caveat)"}}>Vixcera</p></div>
         </div>
