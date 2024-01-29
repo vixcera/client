db02efa687bc4b00c8c3ddb6be85a7e7666a7ee0
vixcera <vixceraa@gmail.com>
2024-01-29 18:28:19 +0700

    update vixcera

--- utils/swaload.jsx
+++ utils/swaload.jsx
@@ -9,8 +9,8 @@ const Swaload = {
             <div className='product-card' key={index}>
                 <Skeleton style={{boxShadow: 'var(--softshadow)'}} className='product-img' baseColor='var(--primary)' highlightColor='var(--prime)'/>
                 <div className='wrapped-text'>
-                    <Skeleton  style={{boxShadow: 'var(--softshadow)'}} className='product-title' count={2} baseColor='var(--primary)' highlightColor='var(--prime)'/>
-                    <div className='wrapdet' style={{ position: 'unset', marginTop: '15px', padding: '0', gap: '5px' }}>
+                    <Skeleton  style={{boxShadow: 'var(--softshadow)', marginLeft: '0'}} className='product-title' count={2} baseColor='var(--primary)' highlightColor='var(--prime)'/>
+                    <div className='wrapdet' style={{ position: 'unset', marginTop: '15px', marginLeft: '5px', gap: '5px' }}>
                         <Skeleton style={{boxShadow: 'var(--softshadow)'}} width={95} height={30} baseColor='var(--primary)' highlightColor='var(--prime)'/>
                         <Skeleton style={{boxShadow: 'var(--softshadow)'}} width={95} height={30} baseColor='var(--primary)' highlightColor='var(--prime)'/>
                     </div>
