793f22a0daffcf9dc552e099d1e61266d27344b9
vixcera <vixceraa@gmail.com>
2024-01-29 19:47:30 +0700

    update vixcera

--- service/handle.jsx
+++ service/handle.jsx
@@ -3,12 +3,18 @@ import { LazyLoadImage } from "react-lazy-load-image-component"
 
 const Handle = ({ status }) => {
     return(
-        <div className="page">
+        <div className="page" style={{flexDirection: 'column', gap: '30px'}}>
             <Topback/>
             {(status == 404) ? (
+                <>
                 <LazyLoadImage src="/img/404page.png" effect="blur" loading="lazy" style={{width: '250px'}}/>
+                <div className="title" style={{fontSize: '1.4rem'}}>Products not found</div>
+                </>
             ) : (
+                <>
                 <LazyLoadImage src="/img/200page.png" effect="blur" loading="lazy" style={{width: '250px'}}/>
+                <div className="title" style={{fontSize: '1.4rem'}}>Server maintenance</div>
+                </>
             )}
         </div>
     )
