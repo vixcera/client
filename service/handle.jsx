18046c2eda9c4f1b8cf53eb63471552fd8317a78
vixcera <vixceraa@gmail.com>
2024-01-31 01:06:35 +0700

    update vixcera

--- service/handle.jsx
+++ service/handle.jsx
@@ -11,7 +11,7 @@ const Handle = ({ status }) => {
                 </>
             ) : (
                 <>
-                <img src="/img/500.png" style={{width: '330px'}}/>
+                <img src="/img/500.png" style={{width: '350px'}}/>
                 </>
             )}
         </div>
