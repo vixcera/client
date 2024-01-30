13972a6bddeb634f644f04e1492bf31f47ef68c7
vixcera <vixceraa@gmail.com>
2024-01-30 23:05:16 +0700

    update vixcera

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -66,7 +66,7 @@ const Product = () => {
                         })
                     )}
                 </div>
-                <div style={{ display: 'flex', gap: '30px', margin: '20px auto' }}>
+                <div style={{ display: 'flex', gap: '20px', marginTop: '30px', alignItems: 'center' }}>
                     <div className='button'><div className='fa-solid fa-left-long fa-xl'/></div>
                     <div className='button'><div className='fa-solid fa-right-long fa-xl'/></div>
                 </div>
