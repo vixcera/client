980818475b3090a64dd3f63ec760ee9c7e945bc6
vixcera <vixceraa@gmail.com>
2024-02-01 13:28:19 +0700

    update vixcera

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -73,7 +73,7 @@ const Product = () => {
                     </div>
                 :
                     <div style={{ display: 'flex', gap: '20px', marginTop: '50px', alignItems: 'center', justifyContent: 'center' }}>
-                        <div className='desc' style={{fontFamily: 'var(--mono)',fontSize: '0.9rem', color: 'var(--primary)'}}>already displays all products.</div>
+                        <div className='desc' style={{fontFamily: 'var(--mono)',fontSize: '0.9rem', color: 'var(--text)'}}>already displays all products.</div>
                     </div>
                 }
             </div>
