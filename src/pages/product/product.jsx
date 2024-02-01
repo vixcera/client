2eca0af2fd9e6c3979ec2281ba7393914527ed32
vixcera <vixceraa@gmail.com>
2024-02-01 21:58:30 +0700

    update vixcera

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -33,7 +33,7 @@ const Product = () => {
     return (
         <div className='page-max'>
             <div id='snap-container'></div>
-                <div className="back" onClick={() => navigate(-1)}>
+                <div className="back" onClick={() => navigate('/products')}>
                     <div className="fa-solid fa-arrow-left fa-xl active"></div>
                     <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
                 </div>
