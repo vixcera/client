3d561838d32b61948209555a7d8a54946b72f53c
vixcera <vixceraa@gmail.com>
2024-02-01 14:48:33 +0700

    update vixcera

--- src/pages/product/order.jsx
+++ src/pages/product/order.jsx
@@ -78,7 +78,7 @@ const Order = () => {
 
     return (
         <div className='page-max' style={{gap:'30px'}}>
-          <div className="back" onClick={() => navigate(`/product/details/${vid}`)}>
+          <div className="back" onClick={() => navigate(`/product/details/${vid}`, { state: i })}>
             <div className="fa-solid fa-arrow-left fa-xl active"></div>
             <div className="nav-logo" style={{fontFamily: 'var(--caveat)'}}>Vixcera</div>
             <div className='snap-container'></div>
