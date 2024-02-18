153aad8082d03fc30507f18cb9abd2cc97768e23
vixcera <vixceraa@gmail.com>
2024-02-18 23:48:36 +0700

    update vixcera

--- middleware/authTransaction.jsx
+++ middleware/authTransaction.jsx
@@ -78,20 +78,20 @@ const AuthTransaction = () => {
             <div className='button-max' onClick={() => { data.transaction_status == 'settlement' && donwloadProduct() }} style={data.transaction_status == 'settlement'? { backgroundColor: 'var(--yellow)' } : {backgroundColor: '#aaa'}}>Download File</div>
             <div style={{width: '100%', display: 'flex', gap: '5px', fontFamily: 'var(--quicksand)'}}>
                 <div style={{width: '50%', display: 'flex', flexDirection: 'column', gap: '5px', color: 'var(--yellow)'}}>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Customer :</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Status :</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Order ID :</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Product ID :</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Amount :</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Token :</h3>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Customer :</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Status :</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Order ID :</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Product ID :</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Amount :</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>Token :</h4>
                 </div>
                 <div style={{width: '50%', display: 'flex', flexDirection: 'column', gap: '5px', color: 'var(--blue)'}}>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.name}</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.transaction_status}</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.order_id}</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.product_id}</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.product_amount ? convertPrice(data.product_amount) : '***'}</h3>
-                    <h3 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.transaction_token? data.transaction_token.substring(0,10) + "***" : '***'}</h3>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.name}</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.transaction_status}</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.order_id}</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.product_id}</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.product_amount ? convertPrice(data.product_amount) : '***'}</h4>
+                    <h4 style={{border: '1px solid var(--blue)', padding: '7px', borderRadius: '5px'}}>{data.transaction_token? data.transaction_token.substring(0,10) + "***" : '***'}</h4>
                 </div>
             </div>
             <div className='title' style={{textAlign: 'center'}}>Invoice Status</div>
