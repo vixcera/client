94ab46a60ed55d97dbfc47a78b7bddf2047c1548
vixcera <vixceraa@gmail.com>
2024-02-19 01:57:37 +0700

    update vixcera

--- middleware/authTransaction.jsx
+++ middleware/authTransaction.jsx
@@ -87,7 +87,7 @@ const AuthTransaction = () => {
           <div className='form invoice' style={{justifyContent: 'center',  gap: '30px', textAlign: 'left'}}>
             {data.transaction_status == 'settlement' && <div className='button-max' onClick={() => { donwloadProduct() }} style={{ backgroundColor: 'var(--yellow)' }}>Get product file</div>}
             {data.transaction_status == 'pending' && <div className='button-max' onClick={() => repay()} style={{ backgroundColor: 'var(--yellow)' }}>Pay now</div>}
-            <p style={{color: 'var(--blue)', textAlign: 'center', cursor: 'pointer'}}>*Screenshot if needed</p>
+            {data.transaction_status == 'settlement' && <p style={{color: 'var(--blue)', textAlign: 'center', cursor: 'pointer'}}>*Screenshot if needed</p>}
             <div style={{width: '100%', display: 'flex', gap: '5px', fontFamily: 'var(--quicksand)'}}>
                 <div style={{width: '50%', display: 'flex', flexDirection: 'column', gap: '5px', color: 'var(--yellow)'}}>
                     <h4 style={{border: '1px solid var(--blue)', padding: '10px', borderRadius: '5px'}}>Customer :</h4>
