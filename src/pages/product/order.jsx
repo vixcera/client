da59560b90c161558ab27dfb964ae5e14a62bd03
vixcera <vixceraa@gmail.com>
2024-02-22 00:03:40 +0700

    update vixcera

--- src/pages/product/order.jsx
+++ src/pages/product/order.jsx
@@ -134,7 +134,7 @@ const Order = () => {
             <div className='input-form' >
               <div>
                 <div>Name :</div>
-                <input className='productinput' value={name} type="text" placeholder='input your name' onChange={(e) => setName(e.target.value)}  required/>
+                <input className='productinput' value={name} type="text" placeholder='input your name' onChange={(e) => setName(e.target.value)} readOnly required/>
               </div>
               <div>
                 <div>Phone Number :</div>
@@ -142,7 +142,7 @@ const Order = () => {
               </div>
               <div>
                 <div>Email :</div>
-                <input className='productinput' value={email} type="email" placeholder='input your email' onChange={(e) => setEmail(e.target.value)}  required/>
+                <input className='productinput' value={email} type="email" placeholder='input your email' onChange={(e) => setEmail(e.target.value)} readOnly required/>
               </div>
               <div className='button-max' onClick={() => showPlaceOrder()} style={(name && phone && email) ? { backgroundColor: 'var(--yellow)' } : { backgroundColor: "#aaa" }}>Checkout</div>
             </div>
